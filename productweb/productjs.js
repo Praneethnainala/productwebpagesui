88854eed41e4186ad6b3a1f52e7ecc6cab12881d
Praneethnainala <p.nainala@manuhindia.com>
2022-01-13 16:02:17 +0530

    commmit

---
+++ productweb/productjs.js
@@ -0,0 +1,10 @@
+function validatedata() {
+  let x = document.forms["myForm"]["username"].value;
+  if (x == "") {
+	  alert("username must be filled out");
+    return false;
+  }
+}
+function sub(){
+	alert(product Details);
+}
